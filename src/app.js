3dba0108d58c56cf2f49776ba6243877ea05b9fd
Alexis Arango <134032888+AlexisArangoo@users.noreply.github.com>
2023-09-05 15:11:54 -0500

    db_todos

---
+++ src/app.js
@@ -0,0 +1,91 @@
+import express from 'express'
+import db_todos from './util/db_todos.js'
+import Todo from './models/todos.model.js'
+
+Todo
+
+const PORT = process.env.PORT ?? 8000
+
+db_todos.authenticate()
+    .then(() => console.log('Conexion correcta'))
+    .catch(err => console.log(err))
+
+db_todos.sync()
+    .then(() => console.log('base de datos sincronizada'))
+    .catch(err => console.log(err))
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('OK')
+})
+
+
+//GET todos
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.findAll()
+        res.json(todos)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//GET todo by id
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const todo = await Todo.findByPk(id)
+        res.json(todo).end()
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//POST todo
+app.post('/todos', async (req, res) => {
+    try {
+        const {body} = req
+
+        const todo = await Todo.create(body)
+        res.status(201).json(todo)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//PUT todo
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const {body} = req
+
+        await Todo.update(body, {
+            where: {id}
+        })
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//DELETE todo
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        
+        await Todo.destroy({
+            where: {id}
+        })
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.listen(PORT, () => {
+console.log(`Se esta ejecutando el servidor ${PORT}`)
+})
---
+++ src/models/todos.model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import db_todos from "../util/db_todos.js";
+
+const Todo = db_todos.define('todos', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    title: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.STRING
+    },
+
+    completed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+})
+
+export default Todo
